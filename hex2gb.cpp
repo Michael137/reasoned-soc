875b9a564dceaeffa32bb44ac20f2d601369ca32
Michael Buch <michaelbuch12@gmail.com>
2020-02-12 02:38:32 -0500

    change pretty print

--- hex2gb.cpp
+++ hex2gb.cpp
@@ -1,6 +1,8 @@
+#include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <string>
+#include <tuple>
 
 // Calculates memory range given in hex.
 //
@@ -11,27 +13,86 @@
 //     0x95300000-0x17e3bffff = 3.9 GB
 //     95300000-17e3bffff = 3.9 GB
 
-int main(int argc, char* argv[])
+void print_usage()
 {
-	std::string in;
+	std::cout << "Usage: \n"
+	             "\t By memory map: hex2gb -f filename\n"
+	             "\t By range: hex2gb -r <from hex>-<to hex>\n"
+	          << std::endl;
+}
 
-	if(argc == 2) {
-		in = argv[1];
-	} else {
-		std::cout << "Hex Range: " << std::endl;
-		std::cin >> in;
-	}
+std::tuple<std::string, std::string> split2( std::string str,
+                                             char delim = '\n' )
+{
+	size_t delim_pos = str.find( delim );
+	return std::make_tuple( str.substr( 0, delim_pos ),
+	                        str.substr( delim_pos + 1, str.length() ) );
+}
 
-	size_t delim_pos = in.find( '-' );
-	std::string from = in.substr( 0, delim_pos );
-	std::string to   = in.substr( delim_pos + 1, in.length() );
+double hex2bytes( std::string str )
+{
+	auto [from, to] = split2( str, '-' );
 
 	auto from_hex = std::stoull( from, 0, 16 );
 	auto to_hex   = std::stoull( to, 0, 16 );
 
-	std::cout << std::setprecision( 4 )
-	          << ( static_cast<double>( ( to_hex - from_hex ) ) / 1e9 ) << " GB"
-	          << std::endl;
+	return static_cast<double>( ( to_hex - from_hex ) );
+}
+
+std::string prettify_bytes( double bytes )
+{
+	std::string units[] = {"B", "KB", "MB", "GB"};
+	int unit_ptr        = 0;
+	while( bytes >= 1000 )
+	{
+		bytes /= 1000;
+		if( ++unit_ptr == sizeof( units ) / sizeof( units[0] ) )
+			break;
+	}
+
+	std::stringstream ss;
+	ss << std::setprecision( 4 ) << bytes << " " << units[unit_ptr];
+	return ss.str();
+}
+
+void iter_mmap( std::string filename )
+{
+	std::ifstream is{filename};
+	std::string line;
+	std::string str_range, device_name;
+
+	while( getline( is, line ) )
+	{
+		std::tie( str_range, device_name ) = split2( line, ':' );
+		std::cout << device_name << ": ";
+		std::cout << prettify_bytes( hex2bytes( str_range ) ) << std::endl;
+	}
+	std::cout << std::endl;
+}
+
+int main( int argc, char* argv[] )
+{
+	if( argc == 3 )
+	{
+		std::string flag{argv[1]};
+		if( flag == "-f" )
+		{
+			iter_mmap( argv[2] );
+		}
+		else if( flag == "-r" )
+		{
+			std::cout << prettify_bytes( hex2bytes( argv[2] ) ) << " GB"
+			          << std::endl;
+		}
+		else
+		{
+			print_usage();
+		}
+	}
+	else
+	{
+		print_usage();
+	}
 
 	return 0;
 }
