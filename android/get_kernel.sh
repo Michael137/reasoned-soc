66716d463df4f4a8ebdb976e3051c50d20bb35cf
Michael137 <michaelbuch12@gmail.com>
2020-04-19 15:49:46 -0400

    tweak script

--- android/get_kernel.sh
+++ android/get_kernel.sh
@@ -1,12 +1,13 @@
-scp -i ~/.ssh/chips mbuch@acc40.int.seas.harvard.edu:/group/brooks/mbuch/kernels/msm/out/android-msm-pixel-4.9/dist/Image.lz4
+scp -i ~/.ssh/chips mbuch@acc40.int.seas.harvard.edu:/group/brooks/mbuch/kernels/msm/out/android-msm-pixel-4.9/dist/Image.lz4 .
 
-HASH=sha256sum Image.lz4
-KERNEL_DIRNAME="msm-android10-${HASH}"
+RAND=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '')
+KERNEL_DIRNAME="msm-android10-${RAND}"
 
-mkdir ${KERNEL_DIRNAME}
+mkdir "${KERNEL_DIRNAME}"
 
-mv Image.lz4 ${KERNEL_DIRNAME}
+mv Image.lz4 "${KERNEL_DIRNAME}/"
 
 scp -i ~/.ssh/chips mbuch@acc40.int.seas.harvard.edu:/group/brooks/mbuch/kernels/msm/out/android-msm-pixel-4.9/dist/*.ko ${KERNEL_DIRNAME}/
 
 echo "Copied to: ${KERNEL_DIRNAME}"
+
